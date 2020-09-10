93460cf9103d4eafc292e13157503edd3eb92ec7
kenchen <independentchen@gmail.com>
2020-09-10 16:24:34 +0800

    Feat: prevent duplicate crawling

--- router/index.js
+++ router/index.js
@@ -3,17 +3,36 @@ const cheerio = require('cheerio');
 const router = require('express').Router();
 const Redis = require('ioredis');
 const redis = new Redis();
-
+const { promisify } = require('util');
+const wait = promisify(setTimeout);
 
 router.get('/', async (req, res, next) => {
-  const result = await redis.get("result");
-  if (result == null) {
-    const rawData = await axios.get("http://ironman-12th.kenchenisme.com/");
-    const $ = cheerio.load(rawData.data);
-    const data = $('pre').text();
-    await redis.setex('result', 100, data);
-    return res.send(JSON.parse(data));
+  const fetchData = async () => {
+    const request_in_progress = await redis.get('indexRoute');
+    if (request_in_progress == null) {
+      await redis.set('indexRoute', "true");
+      let data;
+      const redisData = await redis.get("result");
+      if (redisData == null) {
+        console.log("No redis data");
+        const rawData = await axios.get("http://ironman-12th.kenchenisme.com/");
+        await wait(15000);
+        const $ = cheerio.load(rawData.data);
+        data = $('pre').text();
+        await redis.setex('result', 10, data);
+      } else {
+        data = redisData;
+      }
+      await redis.del('indexRoute');
+      return data;
+    } else {
+      console.log("The request is in progress. Wait a moment!");
+      await wait(2500);
+      return await fetchData.call(this);
+    }
   }
+
+  const result = await fetchData();
   res.append('X-RateLimit-Limit', req.rateLimit);
   res.append('X-RateLimit-Remaining', req.requestRemains);
   res.status(200).send(JSON.parse(result));
