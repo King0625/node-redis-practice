ebb80e157cab1d7fdf3a3f49a6d838a7bd7aea58
kenchen <independentchen@gmail.com>
2020-09-11 14:27:16 +0800

    Feat: add web scraping lock

--- router/index.js
+++ router/index.js
@@ -9,27 +9,27 @@ const wait = promisify(setTimeout);
 router.get('/', async (req, res, next) => {
   const fetchData = async () => {
     const request_in_progress = await redis.get('indexRoute');
-    if (request_in_progress == null) {
-      await redis.setex('indexRoute', 30, "true");
-      let data;
-      const redisData = await redis.get("result");
-      if (redisData == null) {
-        console.log("No redis data");
-        const rawData = await axios.get("http://ironman-12th.kenchenisme.com/");
-        await wait(15000);
-        const $ = cheerio.load(rawData.data);
-        data = $('pre').text();
-        await redis.setex('result', 10, data);
-      } else {
-        data = redisData;
-      }
-      await redis.del('indexRoute');
-      return data;
-    } else {
+    if (request_in_progress != null) {
       console.log("The request is in progress. Wait a moment!");
       await wait(2500);
       return await fetchData.call(this);
     }
+
+    await redis.setex('indexRoute', 30, "true");
+    let data;
+    const redisData = await redis.get("result");
+    if (redisData == null) {
+      console.log("No redis data");
+      const rawData = await axios.get("http://ironman-12th.kenchenisme.com/");
+      await wait(15000);
+      const $ = cheerio.load(rawData.data);
+      data = $('pre').text();
+      await redis.setex('result', 10, data);
+    } else {
+      data = redisData;
+    }
+    await redis.del('indexRoute');
+    return data;
   }
 
   const result = await fetchData();
